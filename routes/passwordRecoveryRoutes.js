96a4e8af6d746817c1740a7bf14f7ad40cc2f3ec
MD. MONIRUZZAMAN <monir.cse6.bu@gmail.com>
2024-10-13 02:05:44 +0600

    eliminnite comments

--- routes/passwordRecoveryRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { requestPasswordRecovery, resetPassword } = require('../controllers/passwordRecoveryController');
-
-router.post('/password-recovery', requestPasswordRecovery);
-router.post('/reset-password/:token', resetPassword);
-
-module.exports = router;
